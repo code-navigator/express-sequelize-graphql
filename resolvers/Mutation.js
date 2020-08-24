6ac93a76b242f390d1a5eb84b34a6eb6ba7eb019
code-navigator <markbdean1@gmail.com>
2020-08-23 18:41:44 -0700

    Add graphql input types

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,10 +1,12 @@
 import { uuid } from 'uuidv4'
 
 const Mutation = {
-  createUser(parent, args, { User }, info) {
+  createUser(parent, { data }, { User }, info) {
+    console.log(data)
+
     const user = {
       id: uuid(),
-      ...args
+      ...data
     }
 
     return User.createUser(user)
@@ -20,14 +22,14 @@ const Mutation = {
 
     return user.dataValues
   },
-  async updateUser(parent, args, { User }, info) {
-    let user = await User.fetchOneUser({ id: args.id })
+  async updateUser(parent, { id, data }, { User }, info) {
+    let user = await User.fetchOneUser({ id })
 
     if (!user) {
       throw new Error('User not found.')
     }
 
-    user = await User.updateUser(args.id, { ...args })
+    user = await User.updateUser(id, { ...data })
 
     return user[1].dataValues
   }
